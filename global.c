34df26bb7c22cc0633aa5b079bb0321923b15e20
frank2dai <frank2dai@gmail.com>
2020-11-10 18:34:56 +0800

    Add files via upload

---
+++ global.c
@@ -0,0 +1,27 @@
+/*
+ * File:   global.c
+ * Author: Administrator
+ *
+ * Created on November 3, 2020, 6:09 PM
+ */
+
+
+#include <xc.h>
+#include "global.h"
+
+ 
+//定义全局变量
+int global_num1,global_num2;
+ 
+//定义函数
+int func(int m  ,int n )
+{
+    int sum;
+    sum = m+n;
+    return sum;
+}
+
+//void main(void) 
+//{
+//    return;
+//}
---
+++ global.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   global.h
+ * Author: Administrator
+ *
+ * Created on November 3, 2020, 6:06 PM
+ */
+
+#ifndef GLOBAL_H
+#define	GLOBAL_H
+
+//声明在global.c中定义的变量
+extern int global_num1,global_num2;
+
+//声明函数
+int func(int input1, int input2);
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GLOBAL_H */
+
