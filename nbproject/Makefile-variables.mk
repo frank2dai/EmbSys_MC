7d6e06f551b4f639e5a4cb25bd1ebc8e8375fb08
frank2dai <frank2dai@gmail.com>
2020-11-10 18:29:26 +0800

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=My8F-5AW_key_OK.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/My8F-5AW_key_OK.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=my8f-5awkeyok.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/my8f-5awkeyok.x.tar
