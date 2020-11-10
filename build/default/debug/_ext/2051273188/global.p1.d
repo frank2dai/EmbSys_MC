9100b75c483ea02ac3820da2445a6956e688bd97
frank2dai <frank2dai@gmail.com>
2020-11-10 18:32:55 +0800

    Add files via upload

---
+++ build/default/debug/_ext/2051273188/global.p1.d
@@ -0,0 +1,3 @@
+build/default/debug/_ext/2051273188/global.p1:  \
+C:/Users/Administrator/MPLABXProjects/My8F-5AW_key_29.X/My8F-5AW_key_OK.X/global.c  \
+C:/Users/Administrator/MPLABXProjects/My8F-5AW_key_29.X/My8F-5AW_key_OK.X/global.h 
---
+++ build/default/production/_ext/2051273188/global.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/2051273188/global.p1:  \
+C:/Users/Administrator/MPLABXProjects/My8F-5AW_key_29.X/My8F-5AW_key_OK.X/global.c  \
+C:/Users/Administrator/MPLABXProjects/My8F-5AW_key_29.X/My8F-5AW_key_OK.X/global.h 
